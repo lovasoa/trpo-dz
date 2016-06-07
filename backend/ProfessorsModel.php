468e7258395e0797427d0ad8cd7ed4131d033b03
Ophir LOJKINE <pere.jobs@gmail.com>
2016-06-07 04:23:06 +0300

    beautifull sequence diagram

--- backend/ProfessorsModel.php
+++ backend/ProfessorsModel.php
@@ -1,11 +1,21 @@
 <?php
 /**
 * Represent a list of professors. Implements MogetOtvetchat, and thus can answer
-* queries about professors
+* queries about professors.
+* Part of the chain of responsability.
 * \msc
-*    professor,next;
+*    LayerCommunication,professor,next;
 *    |||;
-*    professor box professor [label="ProfessorsModel object"];
+*    professor box professor [label="ProfessorsModel object"], next box next [label="ProfessorsModel object"];
+*    LayerCommunication=>professor [label="otvechat($professor_zaproc)"];
+*    professor=>professor [label="getValue($professor_zaproc)", url="\ref BasicModel"];
+*    LayerCommunication<<professor [label="return string"];
+*    |||;
+*    LayerCommunication=>professor [label="otvechat($drugoï_zaproc)"];
+*    professor=>next [label="next->otvechat($drugoï_zaproc)"];
+*    next=>next [label="getValue($drugoi_zaproc)"];
+*    professor<<next [label="return string"];
+*    LayerCommunication<<professor [label="return string"];
 *  \endmsc
 */
 class ProfessorsModel extends BasicModel {
